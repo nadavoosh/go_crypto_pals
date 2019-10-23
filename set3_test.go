f3dd748b7e9c0cc550f27674fe09773ddeb6e74a
Nadav Recca <nadav@blend.com>
2019-10-23 17:45:28 -0600

    ok

--- set3_test.go
+++ set3_test.go
@@ -2,6 +2,7 @@ package cryptopals
 
 import (
 	// "math/rand"
+	"fmt"
 	"testing"
 )
 
@@ -99,5 +100,64 @@ func TestCTRCipher(t *testing.T) {
 		t.Errorf("Encrypt returned: %s, want %s", c.ciphertext, cipherterxt)
 		return
 	}
-
 }
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func TestBreakCTRWithGuessing(t *testing.T) {
+	filename := "challenges/challenge19.txt"
+	lines, err := ScanFile(filename)
+	if err != nil {
+		t.Errorf("ScanFile threw an error: %s", err)
+		return
+	}
+	nonce := int64(0)
+	 // randomly generated:
+	key := []byte{18, 39, 184, 124, 192, 76, 210, 222, 7, 118, 111, 129, 173, 147, 95, 187}
+	// guessed iteratively by looking at what prints and finding one letter to try next:
+	keystream_guess := []byte{61, 119, 199, 221, 251, 12, 179, 47, 28, 48, 171, 47, 152, 235, 153, 236, 113, 47, 144, 28, 151, 200, 54, 228, 104, 190, 165, 111, 120, 237, 239, 125, 179, 228, 122, 201, 172, 60}
+	
+	for _, plaintext_line := range lines {
+		decoded, err := ParseBase64(plaintext_line)
+		if err != nil {
+			t.Errorf("ReadBase64File(%q) threw an error: %s", filename, err)
+		}
+		d := PlainText{
+			plaintext: decoded,
+			key:       key,
+			nonce:     nonce,
+		}
+		c, err := Encrypt(CTC, d)
+		if err != nil {
+			t.Errorf("Encrypt(%q) threw an error: %s", filename, err)
+		}
+		l := min(len(decoded), len(keystream_guess))
+		plaintext_bytes := FlexibleXor(keystream_guess[:l], c.ciphertext)
+		fmt.Println(plaintext_bytes[l:])
+		fmt.Println(string(plaintext_bytes))
+
+	}
+	return
+}
+
+// func TestBreakCTRStatistically(t *testing.T) {
+// 	filename := "challenges/challenge20.txt"
+// 	decoded, err := ReadBase64File(filename)
+// 	if err != nil {
+// 		t.Errorf("ReadBase64File(%q) threw an error: %s", filename, err)
+// 	}
+// 	key := []byte("YELLOW SUBMARINE")
+// 	in := EncryptedText{key: key, ciphertext: decoded, iv: RepeatBytesToLegnth([]byte{0}, aes.BlockSize)}
+// 	got, err := decryptCBC(in)
+// 	if err != nil {
+// 		t.Errorf("decryptCBC(%q) threw an error: %s", in, err)
+// 	}
+// 	if !TestEq(got.plaintext, RemovePKCSPadding([]byte(FunkyMusic))) {
+// 		t.Errorf("encryptCBC(%q) == %q, want %q", in, got, FunkyMusic)
+// 	}
+// }
